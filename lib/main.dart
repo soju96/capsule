eef8843d29756e1e5d13d32d2ae70529cd21438b
LEEKYEOLE <dlrufp1213@naver.com>
2024-02-08 17:26:23 +0900

    [kl] branch check

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'kyeole check',
+      title: 'kl branch check',
       theme: ThemeData(
         // This is the theme of your application.
         //

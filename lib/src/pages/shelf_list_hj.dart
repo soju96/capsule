d9df38a4255cef1a90459894854fc890ad45cf53
heji <slothtree@gmail.com>
2024-02-14 18:26:32 +0900

    [hj] /update / 선반 리스트 화면 구현중

--- lib/src/pages/shelf_list_hj.dart
+++ lib/src/pages/shelf_list_hj.dart
@@ -1,4 +1,5 @@
 // 홈 눌렀을때 넘어오는 페이지
+import 'package:capsule/src/widgets/custom_bottom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class ShelfList extends StatelessWidget {
@@ -7,78 +8,46 @@ class ShelfList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Stack(
-                  children: [
-                    Row(
-                      children: [
-                        Image.asset(
-                          'assets/images/bottle.png',
-                          width: 300,
-                        ),
-                        const SizedBox(
-                          width: 30,
-                        ),
-                      ],
-                    ),
-                    Image.asset(
-                      'assets/images/bottle.png',
-                      width: 300,
-                    ),
-                    const SizedBox(
-                      width: 30,
-                    ),
-                    Image.asset(
-                      'assets/images/bottle.png',
-                      width: 300,
-                    ),
-                    const SizedBox(
-                      width: 30,
-                    ),
-                    Image.asset(
-                      'assets/images/bottle.png',
-                      width: 300,
-                    ),
-                    const SizedBox(
-                      width: 30,
-                    ),
-                    Image.asset(
-                      'assets/images/bottle.png',
-                      width: 300,
-                    ),
-                    const SizedBox(
-                      width: 30,
-                    ),
-                    Image.asset(
-                      'assets/images/bottle.png',
-                      width: 300,
-                    ),
-                    const SizedBox(
-                      width: 30,
-                    ),
-                    Image.asset(
-                      'assets/images/bottle.png',
-                      width: 300,
-                    ),
-                    const SizedBox(
-                      width: 30,
-                    ),
-                    Image.asset(
-                      'assets/images/bottle.png',
-                      width: 300,
-                    ),
-                  ],
-                ),
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
               ),
-              Image.asset('assets/images/shelf.png')
-            ],
-          ),
-        ));
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/images/bottle.png',
+                    width: 300,
+                  ),
+                  Image.asset(
+                    'assets/images/bottle.png',
+                    width: 300,
+                  ),
+                  Image.asset(
+                    'assets/images/bottle.png',
+                    width: 300,
+                  ),
+                  Image.asset(
+                    'assets/images/bottle.png',
+                    width: 300,
+                  ),
+                  Image.asset(
+                    'assets/images/bottle.png',
+                    width: 300,
+                  ),
+                ],
+              ),
+            ),
+            Image.asset('assets/images/shelf.png'),
+          ],
+        ),
+      ),
+      bottomNavigationBar: const CustomBottomAppBar(),
+    );
   }
 }

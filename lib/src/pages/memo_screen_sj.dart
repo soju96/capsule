2a76614deecea4146b39fa4d4d07c8f58cbd0d69
sung joo kim <tjdwn49@gmail.com>
2024-02-15 14:51:13 +0900

    [sj]/ add / pages/memo_screen_sj.dart 추가 누락하고 커밋하여 재 커밋

---
+++ lib/src/pages/memo_screen_sj.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// 일단 파일만 만들어 놓고 나중에 업데이트 합니다.
+
+class MemoInput extends StatefulWidget {
+  const MemoInput({super.key});
+
+  @override
+  State<MemoInput> createState() => _MemoInputState();
+}
+
+class _MemoInputState extends State<MemoInput> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
